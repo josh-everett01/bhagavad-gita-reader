7cb1f70c932835196f88c1f91910f83da1d6effc
josh-everett01 <59103702+josh-everett01@users.noreply.github.com>
2020-06-07 18:53:56 -0500

    Add fetch call with improvised programmer quotes

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,20 @@
 function getBhagavadGita() {
-  alert("Your function is working");
+  var quoteResponse = fetch('https://programming-quotes-api.herokuapp.com/quotes/random')
+    .then(response => response.json())
+
+  var mainSection = document.getElementById("main-section");
+  mainSection.innerHTML = "";
+
+  quoteResponse.then(response => {
+    var quoteHeader = document.createElement("h3");
+    console.log(response);
+    var node = document.createTextNode(response.en);
+    quoteHeader.appendChild(node);
+
+    // TODO: create additional text node and HTML element for response.author 
+
+    var element = document.getElementById("main-section");
+    element.appendChild(quoteHeader);
+    // console.log(response.en);
+  });
 }
